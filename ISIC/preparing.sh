c7265fb8116bffb14169d7a1f91dfcd7cc60f9ba
Dominik <dominik.kiefer@t-online.de>
2017-11-27 10:40:44 +0100

    isic preparing.sh updated

--- ISIC/preparing.sh
+++ ISIC/preparing.sh
@@ -14,5 +14,9 @@ unzip ISIC_MSK-2_1_sorted.zip
 
 mv ISIC_MSK-2_1_sorted data
 
+wget "https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+
+git config core.editor "vim"
+
 echo "Preparing done."
 
