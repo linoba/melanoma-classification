90642630f9b5a202fdbaf757d4f76d0d21f5d6bd
Dominik <dominik.kiefer@t-online.de>
2017-12-04 11:43:23 +0100

    renaming

--- ISIC/preparing.sh
+++ ISIC/preparing.sh
@@ -7,7 +7,7 @@ git clone https://github.com/linoba/melanoma-classification
 echo "Downloading done."
 
 echo "Downloading images."
-wget "www.dominikmorgen.de/data/ISIC_MSK-2_1_sorted.zip"
+wget "www.dominikmorgen.de/data/ISIC_MSK-2_1_sorted.zip" # CHANGEME
 echo "Downloading done."
 
 unzip ISIC_MSK-2_1_sorted.zip
