a43daec218e0e246fa2eaeac89190016af8cdd93
Dominik <dominik.kiefer@t-online.de>
2017-12-16 19:12:05 +0100

    isic - 5 updated (plots) and preparing.sh

--- ISIC/preparing.sh
+++ ISIC/preparing.sh
@@ -16,6 +16,8 @@ mv ISIC_MSK-2_1_sorted data
 
 wget "https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5"
 
+mkdir download
+
 cd melanoma-classification 
 git config core.editor "vim"
 
